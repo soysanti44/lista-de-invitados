4f8c070d06750aabe55768f7be0dd879b7120993
soysanti44 <105685445+soysanti44@users.noreply.github.com>
2022-10-03 19:36:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var nombre_del_alumno_areglo =[];
+function submit() {
+var ver_alubno_arreglo =[];
+for (var j=1; j<=4; j++) {
+var nombre_alumno =document.getElementById("nombre_alumno_"+j).value;
+ nombre_del_alumno_areglo .push(nombre_alumno);
+}
+}
